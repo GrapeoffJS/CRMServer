eaeb7dceb5404714de1bed90450250fee93daef5
GrapeoffJS <idmitriy.git@gmail.com>
2021-07-27 19:08:01 +0300

    Returned validation

--- src/crm/pupils/pipes/create-pupil-validation.pipe.ts
+++ src/crm/pupils/pipes/create-pupil-validation.pipe.ts
@@ -10,18 +10,18 @@ import { Genders } from '../models/Genders';
 @Injectable()
 export class CreatePupilValidationPipe implements PipeTransform {
     transform(value: createPupilDTO, metadata: ArgumentMetadata) {
-        // if (
-        //     !value.name ||
-        //     !value.surname ||
-        //     !value.midname ||
-        //     !value.age ||
-        //     !value.gender
-        // ) {
-        //     throw new BadRequestException();
-        // }
+        if (
+            !value.name ||
+            !value.surname ||
+            !value.midname ||
+            !value.age ||
+            !value.gender
+        ) {
+            throw new BadRequestException();
+        }
 
-        // if (value.gender !== Genders.Male && value.gender !== Genders.Female)
-        //     throw new BadRequestException();
+        if (value.gender !== Genders.Male && value.gender !== Genders.Female)
+            throw new BadRequestException();
 
         return value;
     }
