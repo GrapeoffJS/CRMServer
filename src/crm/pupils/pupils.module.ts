87fbddb195383435822d2e6c321a62ec1b86afe9
GrapeoffJS <idmitriy.git@gmail.com>
2021-08-02 02:16:13 +0300

    Removed old controller

--- src/crm/pupils/pupils.module.ts
+++ src/crm/pupils/pupils.module.ts
@@ -1,18 +1,17 @@
 import CRMUser from 'src/crmaccounts/models/CRMUser.model';
 import Pupil from './models/Pupil.model';
 import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
-import { PupilsController } from './pupils.controller';
-import { SearchIndexerModule } from '../../search-indexer/search-indexer.module';
-import { TypegooseModule } from 'nestjs-typegoose';
+import { CrudController } from './controllers/crud/crud.controller';
+import { CrudService } from './services/crud/crud.service';
 import { ImportFileController } from './controllers/import-file/import-file.controller';
 import { ImportFileService } from './services/import-file/import-file.service';
-import { CrudService } from './services/crud/crud.service';
-import { NotesService } from './services/notes/notes.service';
-import { PaymentService } from './services/payment/payment.service';
-import { CrudController } from './controllers/crud/crud.controller';
-import { PaymentController } from './controllers/payment/payment.controller';
+import { Module } from '@nestjs/common';
 import { NotesController } from './controllers/notes/notes.controller';
+import { NotesService } from './services/notes/notes.service';
+import { PaymentController } from './controllers/payment/payment.controller';
+import { PaymentService } from './services/payment/payment.service';
+import { SearchIndexerModule } from '../../search-indexer/search-indexer.module';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
     imports: [
@@ -30,7 +29,6 @@ import { NotesController } from './controllers/notes/notes.controller';
         ConfigModule
     ],
     controllers: [
-        PupilsController,
         ImportFileController,
         CrudController,
         PaymentController,
