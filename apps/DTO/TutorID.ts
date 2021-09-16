95aaaa17ca6d0172db293e43be2fc99dc909449e
RagAndCodeMan <idmitriy.git@gmail.com>
2021-09-16 21:52:09 +0300

    fixed bugs

--- apps/DTO/TutorID.ts
+++ apps/DTO/TutorID.ts
@@ -1,6 +1,7 @@
-import { IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+import { IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class TutorID {
+    @IsOptional()
     @IsNotEmpty()
     @IsString()
     @IsMongoId()
