ff8266989859782b561b8e5d7e1629d4a1c816c6
RagAndCodeMan <idmitriy.git@gmail.com>
2021-09-21 16:56:00 +0300

    added deployment scripts

---
+++ deploy_a.sh
@@ -0,0 +1,4 @@
+sudo rm -rf /usr/locan/nginx/html/admin_panel
+sudo mv admin_panel/ /usr/locan/nginx/html/
+
+ngrestart
---
+++ deploy_c.sh
@@ -0,0 +1,4 @@
+sudo rm -rf /usr/locan/nginx/html/client
+sudo mv client/ /usr/locan/nginx/html/
+
+ngrestart
