851aab96fa9677e7511eb5d217048a26a584afd3
GrapeoffJS <idmitriy.git@gmail.com>
2021-10-09 20:02:40 +0300

    added update route

--- apps/crmserver/src/crm/tasks/tasks.service.ts
+++ apps/crmserver/src/crm/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { CreateTaskDTO } from './DTO/CreateTaskDTO';
 import { InjectModel } from 'nestjs-typegoose';
 import { Task } from './models/Task.model';
 import { ReturnModelType } from '@typegoose/typegoose';
+import { UpdateTaskDTO } from './DTO/UpdateTaskDTO';
 
 @Injectable()
 export class TasksService {
@@ -17,5 +18,10 @@ export class TasksService {
 
     public async delete(id: string) {
         return this.TaskModel.findByIdAndDelete(id);
+    }
+
+    public async update(id: string, updateTaskDTO: UpdateTaskDTO) {
+        await this.TaskModel.updateOne({ _id: id }, updateTaskDTO);
+        return this.TaskModel.findById(id);
     }
 }
