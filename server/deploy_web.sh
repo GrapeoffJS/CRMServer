6f8abaacdfdf419fbf1e53d49bcff64ee49dc999
GrapeoffJS <idmitriy.git@gmail.com>
2021-10-23 18:56:38 +0300

    Remove old client and admin panel only when there are new admin panel and client

--- server/deploy_web.sh
+++ server/deploy_web.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-sudo rm -rf /usr/local/nginx/html/admin_panel
-sudo rm -rf /usr/local/nginx/html/client
-
 if [--d ./admin_panel]; then
+  sudo rm -rf /usr/local/nginx/html/admin_panel
   sudo mv ./admin_panel /usr/local/nginx/html/
 fi
 
 if [--d ./client]; then
+  sudo rm -rf /usr/local/nginx/html/client
   sudo mv ./client /usr/local/nginx/html/
 fi
 
