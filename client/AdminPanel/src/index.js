9f0e0d5744405ec3691d44f670eb323f8bd67351
GrapeoffJS <idmitriy.git@gmail.com>
2021-11-30 18:07:32 +0300

    Added new client

--- client/AdminPanel/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomePage from './components/#home/home-page.js';
-import 'antd/dist/antd.css';
-
-ReactDOM.render(<HomePage/>,document.getElementById('root'));
--- client/AdminPanel/src/url/url.js
+++
@@ -1,5 +0,0 @@
-const Url = 'http://localhost:8010/proxy';
-export default Url;
-
-// admin_panel // http://crm.3place.ru
-// http://crmedu.3place.ru
