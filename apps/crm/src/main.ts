7886c8e77be9fd326cf17471e8b902af2ff4597d
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-23 02:14:48 +0300

    Removed redundant singleton `SearchIndexer` implementation

--- apps/crm/src/main.ts
+++ apps/crm/src/main.ts
@@ -3,13 +3,9 @@ import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { SearchIndexer } from './SearchIndexer/SearchIndexer';
-import getESConnectionUri from './config/getESConnectionUri';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const configService = app.get<ConfigService>(ConfigService);
 
     app.enableCors();
     app.use(helmet());
@@ -20,18 +16,6 @@ async function bootstrap() {
             transform: true
         })
     );
-
-    SearchIndexer.getInstance().connect({
-        node: getESConnectionUri(
-            configService.get('ELASTIC_SEARCH_PROTOCOL'),
-            configService.get('ELASTIC_SEARCH_HOST'),
-            configService.get('ELASTIC_SEARCH_PORT')
-        ),
-        auth: {
-            username: configService.get('ELASTIC_SEARCH_USERNAME'),
-            password: configService.get('ELASTIC_SEARCH_PASSWORD')
-        }
-    });
 
     await app.listen(process.env.PORT || 4200);
 }
