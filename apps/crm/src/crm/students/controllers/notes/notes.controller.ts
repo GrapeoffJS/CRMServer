ceadaff99f576f118f4cad72bd9dfabd1d2a4259
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-23 22:08:42 +0300

    Notes CRUD operations

---
+++ apps/crm/src/crm/students/controllers/notes/notes.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
+import { NotesService } from '../../services/notes/notes.service';
+import { CreateNoteDTO } from '../../DTO/Note/CreateNoteDTO';
+import { MongoID } from '../../../../../../../utils/DTO/MongoID';
+import { StudentID } from '../../DTO/Student/StudentID';
+import { NoteID } from '../../DTO/Note/NoteID';
+import { UpdateNoteDTO } from '../../DTO/Note/UpdateNoteDTO';
+import { NoteModel } from '../../models/Note.model';
+import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('/crm/students/')
+@Controller('/crm/students')
+export class NotesController {
+    constructor(private readonly notesService: NotesService) {}
+
+    @ApiParam({ name: 'id', type: () => String })
+    @ApiBody({ type: () => CreateNoteDTO })
+    @Post(':id/notes')
+    async create(
+        @Param() { id }: MongoID,
+        @Body() createNoteDTO: CreateNoteDTO
+    ): Promise<NoteModel> {
+        return await this.notesService.create(id, createNoteDTO);
+    }
+
+    @ApiParam({ name: 'studentID', type: () => String })
+    @ApiParam({ name: 'noteID', type: () => String })
+    @Patch(':studentID/notes/:noteID')
+    async update(
+        @Param() { studentID }: StudentID,
+        @Param() { noteID }: NoteID,
+        @Body() updateNoteDTO: UpdateNoteDTO
+    ): Promise<NoteModel> {
+        return await this.notesService.update(studentID, noteID, updateNoteDTO);
+    }
+
+    @ApiParam({ name: 'studentID', type: () => String })
+    @ApiParam({ name: 'noteID', type: () => String })
+    @Delete(':studentID/notes/:noteID')
+    async delete(
+        @Param() { studentID }: StudentID,
+        @Param() { noteID }: NoteID
+    ): Promise<NoteModel> {
+        return await this.notesService.delete(studentID, noteID);
+    }
+}
---
+++ apps/crm/src/crm/students/services/notes/notes.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { NoteModel } from '../../models/Note.model';
+import { CreateNoteDTO } from '../../DTO/Note/CreateNoteDTO';
+import { UpdateNoteDTO } from '../../DTO/Note/UpdateNoteDTO';
+import { InjectModel } from 'nestjs-typegoose';
+
+@Injectable()
+export class NotesService {
+    constructor(
+        @InjectModel(NoteModel)
+        private readonly noteModel: ReturnModelType<typeof NoteModel>
+    ) {}
+
+    async create(
+        studentID: string,
+        createNoteDTO: CreateNoteDTO
+    ): Promise<NoteModel> {
+        return this.noteModel.create({
+            owner_id: studentID,
+            ...createNoteDTO
+        });
+    }
+
+    async update(
+        studentID: string,
+        noteID: string,
+        updateNoteDTO: UpdateNoteDTO
+    ): Promise<NoteModel> {
+        const updated = await this.noteModel.findOneAndUpdate(
+            {
+                _id: noteID,
+                owner_id: studentID
+            },
+            updateNoteDTO
+        );
+
+        if (!updated) {
+            throw new NotFoundException();
+        }
+
+        return this.noteModel.findOne({ _id: noteID, owner_id: studentID });
+    }
+
+    async delete(studentID: string, noteID: string): Promise<NoteModel> {
+        const deleted = await this.noteModel.findOneAndDelete({
+            _id: noteID,
+            owner_id: studentID
+        });
+
+        if (!deleted) {
+            throw new NotFoundException();
+        }
+
+        return deleted;
+    }
+}
