9d96eee5c86f9c4c1d86c711db9e2eac45c0928c
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-23 22:01:57 +0300

    Removed bad authorization implementation

--- apps/crm/src/authorization/ExtendedRequest.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from 'express';
-import { CRMUserModel } from '../../../admin-panel/src/crmusers/models/CRMUser.model';
-import { DocumentType } from '@typegoose/typegoose';
-
-export interface ExtendedRequest extends Request {
-    user: DocumentType<CRMUserModel>;
-}
