a54fbd63599891a7d76375599e8aea6a1ca1e3c3
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-24 21:41:38 +0300

    API Health Checker

--- apps/crm/src/health-check/health-check.controller.ts
+++ apps/crm/src/health-check/health-check.controller.ts
@@ -1,20 +1,48 @@
 import { Controller, Get } from '@nestjs/common';
 import {
+    DiskHealthIndicator,
     HealthCheck,
     HealthCheckService,
-    HttpHealthIndicator
+    MemoryHealthIndicator
 } from '@nestjs/terminus';
+import { TypegooseHealthIndicatorService } from './indicators/typegoose-health-indicator/typegoose-health-indicator.service';
+import { ElasticSearchHealthIndicatorService } from './indicators/elastic-search-health-indicator/elastic-search-health-indicator.service';
 
 @Controller('/health-check')
 export class HealthCheckController {
     constructor(
         private health: HealthCheckService,
-        private http: HttpHealthIndicator
+        private typegooseHealthIndicator: TypegooseHealthIndicatorService,
+        private elasticSearchHealthIndicator: ElasticSearchHealthIndicatorService,
+        private memoryHealthIndicator: MemoryHealthIndicator,
+        private diskHealthIndicator: DiskHealthIndicator
     ) {}
 
     @Get()
     @HealthCheck()
     check() {
-        return this.health.check([]);
+        return this.health.check([
+            () => this.typegooseHealthIndicator.isHealthy('Database'),
+            () => this.elasticSearchHealthIndicator.isHealthy('ElasticSearch'),
+            () =>
+                this.memoryHealthIndicator.checkHeap(
+                    'Memory heap',
+                    300 * 1024 * 1024
+                ),
+            () =>
+                this.memoryHealthIndicator.checkRSS(
+                    'Memory RSS',
+                    300 * 1024 * 1024
+                ),
+            () =>
+                this.diskHealthIndicator.checkStorage('Disk health', {
+                    thresholdPercent: 0.5,
+                    path:
+                        process.platform === 'linux' ||
+                        process.platform === 'darwin'
+                            ? '/'
+                            : 'C:'
+                })
+        ]);
     }
 }
--- apps/crm/src/health-check/health-check.module.ts
+++ apps/crm/src/health-check/health-check.module.ts
@@ -2,9 +2,40 @@ import { Module } from '@nestjs/common';
 import { HealthCheckController } from './health-check.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
+import { TypegooseHealthIndicatorService } from './indicators/typegoose-health-indicator/typegoose-health-indicator.service';
+import { ElasticSearchHealthIndicatorService } from './indicators/elastic-search-health-indicator/elastic-search-health-indicator.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import getESConnectionUri from '../config/getESConnectionUri';
 
 @Module({
-    imports: [TerminusModule, HttpModule],
-    controllers: [HealthCheckController]
+    imports: [
+        ElasticsearchModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory(configService: ConfigService) {
+                return {
+                    node: getESConnectionUri(
+                        configService.get('ELASTIC_SEARCH_PROTOCOL'),
+                        configService.get('ELASTIC_SEARCH_HOST'),
+                        configService.get('ELASTIC_SEARCH_PORT')
+                    ),
+                    auth: {
+                        username: configService.get('ELASTIC_SEARCH_USERNAME'),
+                        password: configService.get('ELASTIC_SEARCH_PASSWORD')
+                    }
+                };
+            }
+        }),
+        TypegooseModule.forFeature([]),
+        TerminusModule,
+        HttpModule
+    ],
+    controllers: [HealthCheckController],
+    providers: [
+        TypegooseHealthIndicatorService,
+        ElasticSearchHealthIndicatorService
+    ]
 })
 export class HealthCheckModule {}
