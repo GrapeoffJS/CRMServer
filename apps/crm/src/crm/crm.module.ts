3db0a7f4f9743357cae195ba62f55665e8758128
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-27 13:17:23 +0300

    Removed deprecated implementations

--- apps/crm/src/crm/crm.module.ts
+++ apps/crm/src/crm/crm.module.ts
@@ -1,11 +1,5 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
-import { SalesFunnelModule } from './sales-funnel/sales-funnel.module';
-import { StatusesModule } from './statuses/statuses.module';
-import { TasksModule } from './tasks/tasks.module';
-import { TaskTagsModule } from './task-tags/task-tags.module';
+import { ConfigModule } from '@nestjs/config';
 import { GroupsModule } from './groups/groups.module';
 import { SearchModule } from './search/search.module';
 import { StudentsModule } from './students/students.module';
@@ -13,25 +7,8 @@ import { AuthModule } from '../auth/auth.module';
 
 @Module({
     imports: [
-        JwtModule.registerAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory(configService: ConfigService) {
-                return {
-                    secret: configService.get('JWT_SECRET'),
-                    signOptions: {
-                        expiresIn: configService.get('JWT_LIFETIME')
-                    }
-                };
-            }
-        }),
         ConfigModule,
         AuthModule,
-        SubscriptionsModule,
-        SalesFunnelModule,
-        StatusesModule,
-        TasksModule,
-        TaskTagsModule,
         GroupsModule,
         SearchModule,
         StudentsModule
--- apps/crm/src/crm/tasks/models/TaskTypes.ts
+++
@@ -1,5 +0,0 @@
-export enum TaskTypes {
-    Contact,
-    Call,
-    Other
-}
