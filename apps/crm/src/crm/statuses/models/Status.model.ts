fe16453113aa6315a6c772594d3624624c8bf2d4
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-27 14:22:45 +0300

    Refactoring

--- apps/crm/src/crm/statuses/models/Status.model.ts
+++
@@ -1,9 +0,0 @@
-import { prop } from '@typegoose/typegoose';
-
-export default class StatusModel {
-    @prop({ type: () => String, required: true })
-    name: string;
-
-    @prop({ type: () => String, required: true })
-    color: string;
-}
