00abd8f77f7c7f095b1190e38ce5611435c16ddd
Dmitriy Grape <idmitriy.git@gmail.com>
2022-02-13 02:28:41 +0300

    Added fields to DTO

--- apps/crm/src/crm/tasks/crud/dto/update-task.dto.ts
+++ apps/crm/src/crm/tasks/crud/dto/update-task.dto.ts
@@ -1,8 +1,22 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateTaskDto } from './create-task.dto';
+import { IsBoolean, IsISO8601 } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { DateTime } from 'luxon';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+    @ApiProperty()
+    @IsISO8601()
+    @Transform(prop =>
+        DateTime.fromISO(prop.value).setZone('Europe/Moscow').toISODate()
+    )
     completedOn: string;
+
+    @ApiProperty()
+    @IsBoolean()
     done: boolean;
+
+    @ApiProperty()
+    @IsBoolean()
     archived: boolean;
 }
