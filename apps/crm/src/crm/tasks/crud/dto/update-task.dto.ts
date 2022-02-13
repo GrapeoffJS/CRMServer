b6e55903c410e0f13c5b9035f419c6ac1fed912b
Dmitriy Grape <idmitriy.git@gmail.com>
2022-02-13 18:27:46 +0300

    FIXED: Update params is not optional

--- apps/crm/src/crm/tasks/crud/dto/update-task.dto.ts
+++ apps/crm/src/crm/tasks/crud/dto/update-task.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateTaskDto } from './create-task.dto';
-import { IsBoolean, IsISO8601 } from 'class-validator';
+import { IsBoolean, IsISO8601, IsOptional } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { DateTime } from 'luxon';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+    @IsOptional()
     @ApiProperty()
     @IsISO8601()
     @Transform(prop =>
@@ -12,10 +13,12 @@ export class UpdateTaskDto extends PartialType(CreateTaskDto) {
     )
     completedOn: string;
 
+    @IsOptional()
     @ApiProperty()
     @IsBoolean()
     done: boolean;
 
+    @IsOptional()
     @ApiProperty()
     @IsBoolean()
     archived: boolean;
