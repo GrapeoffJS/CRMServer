c49598eac77f1f5c44a94a348d430681d0eca192
Dmitriy Grape <idmitriy.git@gmail.com>
2022-02-13 18:29:26 +0300

    Added api bearer auth

--- apps/crm/src/crm/tutors/tutors.controller.ts
+++ apps/crm/src/crm/tutors/tutors.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { TutorsService } from './tutors.service';
 import { SetResponseTransformationType } from '../../authorization/set-response-transformation-type.decorator';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { TutorModel } from '../../../../admin-panel/src/crmusers/models/tutor.model';
 import { RightsNotNeeded } from '../../authorization/rights-not-needed.decorator';
 
 @ApiTags('Tutors')
+@ApiBearerAuth()
 @Controller('/crm/tutors')
 export class TutorsController {
     constructor(private readonly tutorsService: TutorsService) {}
