bd14e7533361d3f4c7ed4e271e42c86a0a34de31
Dmitriy Grape <idmitriy.git@gmail.com>
2022-02-17 20:54:31 +0300

    Updated response transformation

--- apps/crm/src/crm/search/searcher/searcher.interceptor.ts
+++ apps/crm/src/crm/search/searcher/searcher.interceptor.ts
@@ -9,6 +9,11 @@ import { map, Observable } from 'rxjs';
 @Injectable()
 export class SearcherInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        return next.handle().pipe(map(response => response.body.hits));
+        return next.handle().pipe(
+            map(searchResult => ({
+                docs: searchResult.body.hits.hits,
+                count: searchResult.body.hits.total.value
+            }))
+        );
     }
 }
